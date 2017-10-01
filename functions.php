24d601ab7cabc7cd89b70cf081c57a072182c7cf
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2017-10-01 18:49:37 +0200

    [theme] Remove wp emoji

--- functions.php
+++ functions.php
@@ -353,6 +353,13 @@ if ( ! function_exists( 'activello_header_search_filter' ) ) {
 	}
 }
 
+// REMOVE WP EMOJI
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
 // Include Epsilon Framework
 require_once 'inc/libraries/epsilon-framework/class-epsilon-autoloader.php';
 $args = array(
